b6a07a762975b17952087e358b43d578d07649e5
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2020-11-19 14:17:06 +0530

    Add files via upload

---
+++ JavaScript Practice/charcount.js
@@ -0,0 +1,3 @@
+var characters = prompt("Please enter the number of characters")
+var char_rem = 140 - characters.length
+alert("You have entered "+characters.length+" characters. You have "+char_rem+" remaining.")
