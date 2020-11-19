1b347e00e798237ad64bd3b6db390d2b452ba906
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2020-11-19 20:59:50 +0530

    Add files via upload

---
+++ JavaScript Practice/getmilk.js
@@ -0,0 +1,23 @@
+function getMilk(money) {
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("You can buy "+ Math.floor(money/1.5)+" bottles")
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+
+getMilk(5)
