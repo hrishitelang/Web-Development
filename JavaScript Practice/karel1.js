5a066b1173fdb26f7d8b85c16558118b9f16c283
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2020-11-19 17:32:48 +0530

    Add files via upload

---
+++ JavaScript Practice/karel1.js
@@ -0,0 +1,23 @@
+/**
+ * Welcome to the Stanford Karel IDE.
+ * This is a free space for you to
+ * write any Karel program you want.
+ **/
+function main(){
+  goToTopRight();
+}
+
+function goToTopRight(){
+turnLeft();
+move();
+move();
+move();
+move();
+turnLeft();
+turnLeft();
+turnLeft();
+move();
+move();
+move();
+move();
+}
