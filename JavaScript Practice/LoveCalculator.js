3777b24843d2825483ccea98eb63a8401f44e780
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2020-11-20 11:54:13 +0530

    Add files via upload

--- JavaScript Practice/LoveCalculator.js
+++ JavaScript Practice/LoveCalculator.js
@@ -3,4 +3,12 @@ prompt("Enter your partner's name:");
 var n = Math.random();
 n = n * 100;
 n = Math.round(n) + 1;
-alert("Your love percentage is: "+n+"%");
+if (n > 70) {
+  alert("Your love percentage is: "+n+"%. You both are such love birds!");
+}
+if (n <= 70 && n > 30) {
+  alert("Your love percentage is: "+n+"%. You both are just okay couples.");
+}
+if (n <= 30 && n > 0) {
+  alert("Your love percentage is: "+n+"%.");
+}
