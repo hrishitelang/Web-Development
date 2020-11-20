1decf6b2263da12fc0a17d81f6c7517eed53611b
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2020-11-20 20:52:14 +0530

    Add files via upload

---
+++ JavaScript Practice/FizzBuzz.js
@@ -0,0 +1,19 @@
+var output = [];
+var a = 1;
+function FizzBuzz(){
+  if (a%3 === 0 && a%5 === 0){
+      output.push('FizzBuzz');
+  }
+  else if (a%3 === 0){
+      output.push('Fizz')
+  }
+  else if (a%5 === 0){
+      output.push('Buzz')
+  }
+  else{
+      console.log(output);
+  }
+  a=a+1;
+}
+
+FizzBuzz();
