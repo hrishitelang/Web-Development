abee53996d44f98b0b1d52510e89eec2df5e4d39
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2020-11-20 17:37:23 +0530

    Add files via upload

---
+++ JavaScript Practice/guestList.js
@@ -0,0 +1,8 @@
+var guestList = ['Hrishi', 'Anushree', 'Shruti', 'Nishita']
+var x = prompt("Please enter your name:")
+if (guestList.includes(x)){
+  alert("Your name is in the guestlist")
+}
+else{
+  alert("Sorry, your name is not in the guestlist")
+}
