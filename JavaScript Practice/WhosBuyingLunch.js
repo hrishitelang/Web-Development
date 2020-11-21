4e4286b2d55f0ca6f7519f0fc7d76a8284d8d5ca
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2020-11-21 10:45:49 +0530

    Add files via upload

---
+++ JavaScript Practice/WhosBuyingLunch.js
@@ -0,0 +1,9 @@
+names = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+function whosPaying(names) {
+/******Don't change the code above*******/
+    //Write your code here.
+    var a = Math.random() * names.length;
+    var b = Math.floor(a);
+    return names[b]+" is going to buy lunch today!";
+/******Don't change the code below*******/
+}
